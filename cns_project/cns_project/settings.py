a5bb555aa72436885373347ffc98cd7a31e2012d
Socheata <inemcheata@gmail.com>
2025-05-29 00:16:19 +0700

    Ready for Render Development

--- cns_project/cns_project/settings.py
+++ cns_project/cns_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-eamt)#^6mqqxb#5i8h_=y+kkxz%!7b0obprwrr-isugoyv^$$6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,9 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
+from pathlib import Path
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
